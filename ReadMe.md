3bf6a2ef0ed09f3c565c68fab0e5900a8e24d4c5
Alexander Lüngen <a-luengen@web.de>
2021-12-13 20:48:14 +0100

    Added cli args to parse arguments.

--- ReadMe.md
+++ ReadMe.md
@@ -10,17 +10,17 @@ from and to csv files, to simplify translation management.
 
 ## 1. Create from react-native-localization conform directory:
 
-itransf --to <directory_path> <target_filepath_csv>
+itransf --to <directory_path> --output <target_filepath_csv>
 
 ## 2. Create from reactjs next-i18next conform directory
 
-itransf --to <file_path> <target_filepath_csv>
+itransf --to <file_path> --output <target_filepath_csv>
 
 ## 3. Transform into react-native-localization conform directory
 
-itransf --from <src_filepath> <target_directory_path>
+itransf --from <src_filepath> --output <target_directory_path>
 
 ## 4. Transform into reactjs next-i18next conform directory
 
-itransf --from <src_filepath_csv> <target_directory_path>
+itransf --from <src_filepath_csv> --output <target_directory_path>
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "i18n-transform",
+    about = "CLI Tool to convert a i18n files and certain related directory structures into single csv files and vice-versa."
+)]
+struct Cli {
+    #[structopt(parse(from_os_str), short = "t", long = "to")]
+    to_file_or_dir: Option<std::path::PathBuf>,
+    #[structopt(
+        parse(from_os_str),
+        short = "o",
+        long = "output",
+        default_value = "./output.csv"
+    )]
+    csv_filepath: std::path::PathBuf,
+    #[structopt(parse(from_os_str), short = "f", long = "from")]
+    from_file_or_dir: Option<std::path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    if let Some(val) = args.to_file_or_dir {
+        println!("{}", val.into_os_string().into_string().unwrap())
+    }
+    println!(
+        "{}",
+        args.csv_filepath.into_os_string().into_string().unwrap()
+    );
+    if let Some(val) = args.from_file_or_dir {
+        println!("{}", val.into_os_string().into_string().unwrap())
+    }
+
+    // determine which mode to run
 }
